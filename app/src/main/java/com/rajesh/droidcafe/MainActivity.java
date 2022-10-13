0d101217e561be61d20db31cf54ecb45145601c5
Rajesh Lakkakula <rajeshlakkakula536@gmail.com>
2022-10-13 21:37:41 +0530

    Initial commit

---
+++ app/src/main/java/com/rajesh/droidcafe/MainActivity.java
@@ -0,0 +1,53 @@
+package com.rajesh.droidcafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton floatingActionButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        floatingActionButton = findViewById(R.id.floatingActionButton);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+
+
+    public void displayToast(String message) {
+
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void showIceCreamOrder(View view) {
+        displayToast("You Ordered IceCream");
+    }
+
+    public void showFroyoOrder(View view) {
+        displayToast("You Ordered Froyo");
+    }
+
+    public void showDoNutOrder(View view) {
+        displayToast("You Ordered DoNut");
+    }
+}
